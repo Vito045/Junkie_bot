d549b8b5a42c7a149a73d1f881e3e6a36915b605
Vito045 <vito045@icloud.com>
2018-09-22 20:49:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,7 @@
  const config = require("./config")
  const kb = require("./keyboard-buttons")
  const keyboard = require("./keyboard")
- const mongoose = require("mongoose")
 
- const EasyPayWallet = "28828833"
- const BitcoinWallet = "680769754:AAFvusu4UO0LBQ8i5dYhZ4vbsw0JCpDr9RU"
-
- helper.logStart()
 const bot = new TelegramBot(config.TOKEN, {
      polling: true
  })
