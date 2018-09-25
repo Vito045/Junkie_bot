1e274507ea402cbb3e7690d8687c454cdc159f25
Vito045 <vito045@icloud.com>
2018-09-25 20:33:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const keyboard = require("./keyboard")
 const EasyPayWallet = "28828833"
 const BitcoinWallet = "13Bi9FsmcFJnFtPJSCkAyXB2cy85YAnrPw"
 
-const TOKEN = "679062113:AAF6VFdV7p4EgHjJ5gb2xmWDzBzBQ8sd1i0"
+const TOKEN = "693386835:AAGBsU6v4cp-e2sffN0u50GOoq-K9RFP77g"
 
 helper.logStart()
 
